3162e7d9196a3d131c2856cdb0b88fc2a1d408fb
etrickel <erik.trickel@asu.edu>
2020-05-17 17:14:19 -0700

    adding solution info

--- rebase_on_template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-echo "NOTE: This will only work if your repository shares history with the template"
-echo "      If not, you may have to cherry-pick (or just ask me to fix it for you)"
-
-git remote add template 'git@github.com:o-o-overflow/dc2020q-template.git' 2>/dev/null || true
-git fetch template
-git rebase --stat template/master
